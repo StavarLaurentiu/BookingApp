05136e96413031a780a0ac0f8ec9d16c38d834b8
bogdanpopaa <bogdan.popa@qualitestgroup.com>
2025-05-03 15:29:29 +0300

    frontend navbar config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,10 @@ import { useContext, useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 
-import { UserContext } from "./components/UserContext";
-
 function App() {
-  const { user, setUser } = useContext(UserContext);
   const [loading, setLoading] = useState(true); // New loading state
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser)); // Parse JSON string from localStorage
-    } else {
-      setUser(null);
-    }
     setLoading(false); // Mark loading as complete
   }, []);
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,13 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import { UserContext } from "./UserContext";
 const Navbar = () => {
-  const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
-  function handleLogout() {
-    localStorage.removeItem("user");
-    setUser(null);
-    navigate("/");
-  }
   return (
     <nav className="navbar">
       <h1>Hotel Booking</h1>
@@ -20,20 +14,6 @@ const Navbar = () => {
         <li>
           <Link to="all-rooms">All Rooms</Link>
         </li>
-        {user == null ? null : (
-          <li>
-            <Link to="my-bookings">My Bookings</Link>
-          </li>
-        )}
-        {user == null ? (
-          <li>
-            <Link to="auth">Login</Link>
-          </li>
-        ) : (
-          <li className="logout" onClick={handleLogout}>
-            Logout
-          </li>
-        )}
       </ul>
     </nav>
   );
