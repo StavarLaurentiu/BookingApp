f98a00d8a71d56a28543a01f7b6ba476dae1684d
bogdanpopaa <bogdan.popa@qualitestgroup.com>
2025-05-05 19:22:02 +0300

    code refactor + dummy data

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import { UserContext } from "./UserContext";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
